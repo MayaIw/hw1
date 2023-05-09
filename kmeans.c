a2a522f8221b1839ea0ca292ad3c0b24769e6497
MayaIw <mayaiwanir@mail.tau.ac.il>
2023-05-09 19:46:24 +0300

    almost finished

--- kmeans.c
+++ kmeans.c
@@ -1,6 +1,18 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <math.h>
+# include <assert.h>
+
+struct cord
+{
+    double value;
+    struct cord *next;
+};
+struct vector
+{
+    struct vector *next;
+    struct cord *cords;
+};
 
 double distance(double *p, double *q)
 {
@@ -27,63 +39,87 @@ int clusterAssign(double *x, double **centroids)
     double min_distance = distance(x, centroids[0]);
     int cluster = 0;
     int i;
+    double dist;
     for(i=1; i<num_of_clusters; i++)
     {
-        double distance = distance(x, centroids[i]);
-        if(distance <= min_distance)
+        dist = distance(x, centroids[i]);
+        if(dist <= min_distance)
         {
-            min_distance = distance;
+            min_distance = dist;
             cluster = i;
         }
     }
     return cluster;
 }
 
-void centroidUpdate(int *cluster, double *centroid, int d, double **elements, int num_of_elements)
+void centroidUpdate(double **clusters, int *size_of_clusters, int k, int d)
 {
     int i = 0;
     int j = 0;
-    int cluster_size = 0;
-    for(j=0; j<d ; j++)
+    for(i=0; i < k; i++)
     {
-        centroid[j] = 0;
-        for(i=0; i < num_of_elements; i++)
+        for(j=0; j<d ; j++)
         {
-            if(cluster[i] == 1)
-            {
-                cluster_size++;
-                centroid[j] += elements[i][j];
-            } 
+            clusters[i][j] /= size_of_clusters[i];
         }
-        centroid[j] /= cluster_size;
     }
+    return;
 }
 
-void increaseArray(double *array, int curr_size)
-{
-    array = realloc(array, 2*curr_size*sizeof(double));
-    assert(array != null);
+void printClusters(double **clusters, int d,  int k){
+    int i=0;
+    int j=0;
+    for(i=0; i<k; i++){
+        for(j=0; j<d; j++){
+            printf("%f", clusters[i][j]);
+            if(j<d-1){
+                printf("%c", ',');
+            }
+        }
+        printf("\n");
+    }
+
+}
+
+void delete_cord( struct cord *head ){
+    if ( head != NULL )
+        {
+            delete_cord( head->next );
+            free( head );
+        }
+    }
+
+void delete_vector(struct vector *head){
+    if(head!=NULL){
+        delete_vector(head->next);
+        delete_cord(head->cords);
+        free(head);
+    }
 }
 
 int main(int argc, char **argv)
 { 
     int k;
     int iter; 
-    int i;
     int num_of_elements=0;
-    int j;
-    int max_line_len;
     int d=0;
-    char ch;
     int is_end_of_line = 0;
-    double coordinate;
-    double *number;
     double *elements_1d;
     double **elements;
     double *centroids_1d;
     double **centroids;
-    int *clusters_1d;
-    int **clusters;
+    double *clusters_1d;
+    double **clusters;
+    int *size_of_clusters;
+    int assigned_centroid;
+    
+    struct vector *head_vec, *curr_vec;
+    struct cord *head_cord, *curr_cord;
+    int i, j, l;
+    double n;
+    char c;
+    struct vector *loop_vector;
+    struct cord *loop_cord;
 
     if (argc != 3) 
     {
@@ -95,38 +131,55 @@ int main(int argc, char **argv)
         iter = atoi(argv[2]);
     }
 
-    while (scanf("%lf%c", &coordinate, &ch)==2)
+    head_cord = malloc(sizeof(struct cord));
+    curr_cord = head_cord;
+    curr_cord->next = NULL;
+
+    head_vec = malloc(sizeof(struct vector));
+    curr_vec = head_vec;
+    curr_vec->next = NULL;
+
+
+    while (scanf("%lf%c", &n, &c) == 2)
     {
-        if(!is_end_of_line)
-        {
+        if(!is_end_of_line){
             d+=1;
-            /*elements_1d = calloc(d, sizeof(double));
-            assert(elements_1d != NULL);
-            elements = calloc(d,sizeof(double *));
-            assert(elements != NULL);    
-            elements[0] = elements_1d;*/
         }
-        
-        if(ch=='\n')
-        {
-            num_of_elements += 1;
-            is_end_of_line = 1;
+        if (c == '\n')
+        {   is_end_of_line =1;
+            curr_cord->value = n;
+            curr_vec->cords = head_cord;
+            curr_vec->next = malloc(sizeof(struct vector));
+            curr_vec = curr_vec->next;
+            curr_vec->next = NULL;
+            head_cord = malloc(sizeof(struct cord));
+            curr_cord = head_cord;
+            curr_cord->next = NULL;
+            num_of_elements++;
+            continue;
         }
-        
-        
+
+        curr_cord->value = n;
+        curr_cord->next = malloc(sizeof(struct cord));
+        curr_cord = curr_cord->next;
+        curr_cord->next = NULL;
     }
-    /*if not empty
-    num_of_elements += 1;
-    max_line_len = 100*d;*/
 
-    /*count number of points in the file
-    *line = (char *)malloc(max_line_len+1 sizeof(char));
-    while(fgets (char *line, int max_line_len, FILE *points))
-    {
-        num_of_elements += 1;
-    }*/
+    if(k<1 || k>num_of_elements){
+        printf("Invalid number of clusters!");
+        /*check if it is an int
+        free allocated memory*/
+        exit(0);
+    }
 
-    rewind(points);
+    if(iter<1 || iter>1000){
+        printf("Invalid maximum iteration!");
+        /*check if it is an int
+        free allocated memory*/
+        exit(0);
+    }
+
+
     /*memory allocation for all the points in the file*/
     elements_1d = calloc(num_of_elements*d, sizeof(double));
     assert(elements_1d != NULL);
@@ -147,37 +200,90 @@ int main(int argc, char **argv)
         centroids[i] = centroids_1d+i*d;
     }
 
-    /*put all the points in one array, and the first k of them in the centroids array*/
+    /*put the first k points in the centroids array*/
     i=0;
-    while(fgets (char *line, int max_line_len, FILE *points))
-    {
-        line[strcspn(line, "\n")] = 0;
-        number = (double *)strtok(line, ',');
-        elements[i] = number;
-        if(i<k){
-            centroids[i] = number;
-        }
-        i++; 
-    } 
-
-    fclose(points);
+    j=0;
+    loop_vector = head_vec;
     
-    /*memory allocation for binary array representing the clusters*/
-    clusters_1d = calloc(k*num_of_elements, sizeof(int));
+    for(i=0; i<k; i++){
+        loop_cord = head_vec->cords;
+        for(j=0; j<d; j++){
+            centroids[i][j]= loop_cord->value;
+            loop_cord=loop_cord->next;
+        }
+        loop_vector=loop_vector->next;
+    }
+    
+    /*memory allocation for sum matrix "representing" clusters*/
+    clusters_1d = calloc(k*d, sizeof(double));
     assert(clusters_1d != NULL);
-    clusters = calloc(num_of_elements,sizeof(int *));
+    clusters = calloc(d,sizeof(double *));
     assert(clusters != NULL);
     for(j=0; j<k; j++)
     {
-        clusters[j] = clusters_1d+j*num_of_elements;
+        clusters[j] = clusters_1d+j*d;
     } 
-    
-    i=0;
-    /*for(i; i<iter; i++)
-    {
 
-    }*/
+    /*memory allocation for array of sizes of clusters*/
+    size_of_clusters = calloc(k, sizeof(int));
     
+    /*assignment of elements into 2d matrix*/
+    i=0;
+    j=0;
+    loop_vector = head_vec;
+    for(i=0; i<num_of_elements; i++){
+        for(j=0; j<d; j++){
+            elements[i][j]= loop_cord->value;
+            loop_cord=loop_cord->next;
+        }
+        loop_vector=loop_vector->next;
+    }
+    delete_vector(head_vec);
+
+    i=0;
+    j=0;
+    l=0;
+    for(i=0; i<iter; i++)
+    {
+       for(j=0; j<num_of_elements; j++){ 
+        assigned_centroid= clusterAssign(elements[j], centroids);
+        for(l=0; l<d; l++){
+            clusters[assigned_centroid][l] += elements[j][l];
+        }
+        size_of_clusters[assigned_centroid]+=1;
+       }
+       centroidUpdate(clusters, size_of_clusters, k, d);
+       j=0;
+       for(j=0; j<k; j++){
+        if(distance(centroids[j],clusters[j])>= 0.001){
+            break;
+        }
+        else if(j==k-1){
+            printClusters(clusters, d, k);
+            i= iter+1;
+            break;
+        }
+       }
+       j=0;
+       l=0;
+       for(j=0;j<k;j++){
+        for(l=0;l<d;l++){
+            centroids[j][l] = clusters[j][l];
+            clusters[j][l]=0;
+        }
+        size_of_clusters[j]=0;
+       }
+    }
+
+    free(elements);
+    free(loop_cord);
+    free(loop_vector);
+    free(centroids);
+    free(centroids_1d);
+    free(elements_1d);
+    free(clusters);
+    free(size_of_clusters);
+    free(clusters_1d);
 
     return 0;
 }
